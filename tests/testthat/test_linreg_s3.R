5cd041ddd9e27e4924bb90f5cf3046d0841d35ea
Siyuan Xu <ningschue@gmail.com>
2025-09-29 14:39:44 +0200

    Add test cases for print() and plot().

--- tests/testthat/test_linreg_s3.R
+++ tests/testthat/test_linreg_s3.R
@@ -8,3 +8,50 @@ test_that("class is correct", {
 
   expect_s3_class(linreg_mod, "linreg")
 })
+
+test_that("print() works", {
+  linreg_mod <- linreg(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
+
+  expect_output(print(linreg_mod),"linreg\\(formula = Petal\\.Length ~ Sepal\\.Width \\+ Sepal\\.Length, data = iris\\)")
+  expect_output(print(linreg_mod),"( )*\\(Intercept\\)( )*Sepal\\.Width( )*Sepal\\.Length")
+})
+
+test_that("plot() throws an error with corrupted or invalid input", {
+
+  linreg_mod <- linreg(Petal.Length ~ Sepal.Width, data = iris)
+
+  # Test for missing components
+  mod_test_1 <- linreg_mod
+  mod_test_1$e_hat <- NULL
+  expect_error(plot(mod_test_1))
+
+  mod_test_2 <- linreg_mod
+  mod_test_2$y_hat <- NULL
+  expect_error(plot(mod_test_2))
+
+  mod_test_3 <- linreg_mod
+  mod_test_3$v_hat <- NULL
+  expect_error(plot(mod_test_3))
+
+  # Test for non-numeric components
+  mod_test_4 <- linreg_mod
+  mod_test_4$e_hat <- "not numeric"
+  expect_error(plot(mod_test_4))
+
+  mod_test_5 <- linreg_mod
+  mod_test_5$y_hat <- "not numeric"
+  expect_error(plot(mod_test_5))
+
+  mod_test_6 <- linreg_mod
+  mod_test_6$v_hat <- "not numeric"
+  expect_error(plot(mod_test_6))
+
+  # Test for invalid properties of v_hat
+  mod_test_7 <- linreg_mod
+  mod_test_7$v_hat <- c(1, 2)
+  expect_error(plot(mod_test_7))
+
+  mod_test_8 <- linreg_mod
+  mod_test_8$v_hat <- -1
+  expect_error(plot(mod_test_8))
+})
